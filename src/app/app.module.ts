7aa08e9bf9bf4fe75fd48ad4e5f49c2bac2fbd3c
Max Griffin <maxwell.griffin@outlook.com>
2019-04-12 17:17:41 -0400

    fun15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { 
+  MatToolbarModule, 
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { FormsModule, ReactiveFormsModule,  } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+
+const routes = [
+  { path: '', component: HomeComponent }, //will default to home when opened
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: '**', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
-  providers: [],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
