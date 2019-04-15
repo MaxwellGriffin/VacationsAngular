27017e10c9b66a389b3893356a3778fbef39b7aa
Debbie <debbieforbeck@gmail.com>
2019-04-15 13:15:54 -0400

    get new information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
-  ],
+    ContactComponent,
+    HeaderComponent
+  ],  
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -21,4 +26,5 @@ import { ContactComponent } from './contact/contact.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
