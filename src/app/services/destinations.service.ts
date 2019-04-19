81229a606424285e23e4d275b431575287a88f08
Max Griffin <maxwell.griffin@outlook.com>
2019-04-19 10:41:52 -0400

    destinationservice fix

--- src/app/services/destinations.service.ts
+++ src/app/services/destinations.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Destination } from '../models/destination';
 import { Api_Url } from 'src/environments/environment.prod';
 
 @Injectable({
@@ -10,11 +11,27 @@ export class DestinationsService {
 
   constructor(private _http: HttpClient) { }
 
-  getDestinations() {
-    return this._http.get(`${Api_Url}/Destination`, { headers: this.getHeaders() });
-  }
-
   private getHeaders(){
     return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+  }
+
+  getDestinations() {
+    return this._http.get(`${Api_Url}/api/Destination`, { headers: this.getHeaders() });
+  }
+
+  getDestination(id: string){
+    return this._http.get(`${Api_Url}/api/Destination/${id}`, { headers: this.getHeaders() });
+  }
+
+  createDestination(destination: Destination){
+    return this._http.post(`${Api_Url}/api/Destination`, destination, { headers: this.getHeaders() });
+  }
+
+  updateDestination(destination: Destination){
+    return this._http.put(`${Api_Url}/api/Destination`, destination, { headers: this.getHeaders() });
+  }
+
+  deleteDestination(id: number){
+    return this._http.delete(`${Api_Url}/api/Destination/${id}`, { headers: this.getHeaders() });
   }
 }
