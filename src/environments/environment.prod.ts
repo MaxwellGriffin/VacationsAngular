83d1a3b50c4d06263d0359acac8e482ee8c050f4
Max Griffin <maxwell.griffin@outlook.com>
2019-05-14 17:45:29 -0400

    changed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,8 @@ export let Api_Url = "";
 //   Api_Url = "https://vacationsunitedwebapi20190422124815.azurewebsites.net";
 //   break;
 // };
-Api_Url = "https://vacationsunitedwebapi20190422124815.azurewebsites.net";
+//Api_Url = "https://vacationsunitedwebapi20190422124815.azurewebsites.net";
+Api_Url = "https://vacationsunitedwebapi.azurewebsites.net";
 
 export const environment = {
   production: true
